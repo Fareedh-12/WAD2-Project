a1c4593789460af0279879138ad87357cfb69438
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-03-10 13:12:52 +0000

    altered forms.py

--- radar_project/radar/forms.py
+++ radar_project/radar/forms.py
@@ -6,7 +6,17 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=128,
+                               help_text="Please enter the username.")
+    # if None or empty string is passed in, then clean() will raise
+    # a ValidationError exception.
+    username.clean("This field is required.")
+
+    email = forms.EmailField()
+    email.clean("invalid email address")
+    
     password = forms.CharField(widget=forms.PasswordInput())
+    password.clean("This field is required.")
 
     class Meta:
         model = User
@@ -14,6 +24,8 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+
+    picture = forms.ImageField
     class Meta:
         model = UserProfile
         fields = ('picture', 'host',)
